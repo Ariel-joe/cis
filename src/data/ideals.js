ece0358f4789101c09414d8e8a3b8312f543561e
Ariel-joe <arieljoe00@gmail.com>
2026-09-11 10:03:57 +0300

    minor-update

--- src/data/ideals.js
+++ src/data/ideals.js
@@ -90,7 +90,7 @@ export const pillars = [
         slug: "parents",
         label: "Parents",
         placeholder: false,
-        media: ["Internationalism/Parents/parent.png"],
+        media: ["Internationalism/Parents/parent.png", "Internationalism/Parents/RSIC-Parents-Feedback.mp4"],
       },
       {
         slug: "governance",
