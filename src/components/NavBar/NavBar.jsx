1286cd035f3cfc79370f060f6232b16088ce1a6b
Ariel-joe <arieljoe00@gmail.com>
2026-09-12 20:12:17 +0300

    navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,22 +1,53 @@
 import { Link } from "react-router-dom";
 import Logo from "../../assets/images/Brookhouse_logo.png";
+import Inspired from "../../assets/images/Inspired_Logo-nobg.png";
+import Roundsquare from "../../assets/images/roundsquare-logo-nobg.png";
 import "./NavBar.css";
 
 export default function NavBar() {
   return (
     <header className="navbar">
       <div className="container navbar__row">
-        <Link to="/" className="navbar__mark">
+        <div className="navbar__left">
+          <a
+            href="https://www.roundsquare.org/"
+            target="_blank"
+            rel="noreferrer"
+            className="navbar__logo-link"
+            aria-label="Round Square (opens in new tab)"
+          >
+            <img
+              className="navbar__image navbar__image--roundsquare"
+              src={Roundsquare}
+              alt="Round Square"
+            />
+          </a>
+          <Link
+            to="/"
+            className="navbar__logo-link"
+            aria-label="Brookhouse Schools home"
+          >
+            <img
+              className="navbar__image navbar__image--brookhouse"
+              src={Logo}
+              alt="Brookhouse Schools"
+            />
+          </Link>
+        </div>
+        <a
+          href="https://www.inspirededu.com/"
+          target="_blank"
+          rel="noreferrer"
+          className="navbar__logo-link"
+          aria-label="Inspired Education (opens in new tab)"
+        >
           <img
-          className="navbar__image"
-          src={Logo}
-          width="210"
-          height="45"
-          alt="navbar logo"
-        />
-        </Link>
-        {/* <span className="navbar__tag">Inspired by the Round Square</span> */}
+            className="navbar__image navbar__image--inspired"
+            src={Inspired}
+            alt="Inspired Education"
+          />
+        </a>
       </div>
-    </header> 
+    </header>
   );
 }
